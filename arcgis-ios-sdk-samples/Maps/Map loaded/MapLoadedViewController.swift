061f475d1008fb6792e27f8a625e5f6d6b67fe21
Nate Whittaker <nwhittaker@esri.com>
2018-05-23 11:13:35 -0400

    Reduce to mapScale assertion (runtime/cocoa-common-api#7519).

--- arcgis-ios-sdk-samples/Maps/Map loaded/MapLoadedViewController.swift
+++ arcgis-ios-sdk-samples/Maps/Map loaded/MapLoadedViewController.swift
@@ -18,63 +18,14 @@ import ArcGIS
 class MapLoadedViewController: UIViewController {
 
     @IBOutlet var mapView:AGSMapView!
-    @IBOutlet var bannerLabel:UILabel!
-    
-    private var map:AGSMap?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //setup source code bar button item
-        (self.navigationItem.rightBarButtonItem as! SourceCodeBarButtonItem).filenames = ["MapLoadedViewController"]
-        
-        //initialize map with basemap
-        self.map = AGSMap(basemap: AGSBasemap.imageryWithLabels())
-        
-        //assign map to map view
-        self.mapView.map = self.map
-        
-        //register as an observer for loadStatus property on map
-        self.map?.addObserver(self, forKeyPath: "loadStatus", options: .new, context: nil)
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        
-        //update the banner label on main thread
-        DispatchQueue.main.async { [weak self] in
-            
-            //get the string for load status
-            if let strongSelf = self, let loadStatus = strongSelf.map?.loadStatus {
-                
-                let loadStatusString = strongSelf.loadStatusString(loadStatus)
-                
-                //set it on the banner label
-                strongSelf.bannerLabel.text = "Load status : \(loadStatusString)"
-            }
-        }
-    }
-    
-    private func loadStatusString(_ status: AGSLoadStatus) -> String {
-        switch status {
-        case .failedToLoad:
-            return "Failed_To_Load"
-        case .loaded:
-            return "Loaded"
-        case .loading:
-            return "Loading"
-        case .notLoaded:
-            return "Not_Loaded"
-        default:
-            return "Unknown"
-        }
+        mapView.map = AGSMap(basemap: AGSBasemap.imageryWithLabels())
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    deinit {
-        self.map?.removeObserver(self, forKeyPath: "loadStatus")
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        assert(!mapView.mapScale.isNaN)
     }
 }
